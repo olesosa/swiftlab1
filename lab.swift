976049154078b29c574c52d1b8b6016a65377275
Олесь <102287993+olesosa@users.noreply.github.com>
2023-10-06 11:24:10 +0300

    Update lab.swift

--- lab.swift
+++ lab.swift
@@ -1,1 +1,25 @@
-efiusdfh
+let s: String? = "/" let a:Int? = 205 let b:Int? = 36
+
+func add(_ num1:Int, _ num2:Int)->Int{ return num1+num2 }
+
+func subt(_ num1:Int, _ num2:Int)->Int{ return num1-num2 }
+
+func multiply(_ num1:Int, _ num2:Int)->Int{ return num1*num2 }
+
+func div(_ num1:Int, _ num2:Int)->Int{ if b != 0 { return num1/num2 } else {return UInt32.max}}
+
+if let s = s, let a = a, let b = b {
+switch s 
+  { case "+": 
+   let result = add(a, b) 
+   print(result) 
+   case "-": 
+   let result = subt(a, b) 
+   print(result) case "*": 
+   let result = multiply(a, b) 
+   print(result) case "/": 
+   let result = div(a, b) 
+   print(result) 
+   default: print("error") 
+  } 
+}
