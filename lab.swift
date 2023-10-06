94fd318154233d0b9bc5b0d45be37f14ac3a6dc3
Олесь <102287993+olesosa@users.noreply.github.com>
2023-10-06 11:44:26 +0300

    Update lab.swift

--- lab.swift
+++ lab.swift
@@ -1,3 +1,7 @@
+enum MyError : Error{
+  case runtimeError(string)
+}
+
 let s: String? = "/" let a:Int? = 205 let b:Int? = 36
 
 func add(_ num1:Int, _ num2:Int)->Int{ return num1+num2 }
@@ -6,7 +10,7 @@ func subt(_ num1:Int, _ num2:Int)->Int{ return num1-num2 }
 
 func multiply(_ num1:Int, _ num2:Int)->Int{ return num1*num2 }
 
-func div(_ num1:Int, _ num2:Int)->Int{ if b != 0 { return num1/num2 } else {return UInt32.max}}
+func div(_ num1:Int, _ num2:Int)->Int throws{ if b != 0 { return num1/num2 } else {throw MyError("error")} return 0}
 
 if let s = s, let a = a, let b = b {
 switch s 
@@ -17,9 +21,10 @@ switch s
    let result = subt(a, b) 
    print(result) case "*": 
    let result = multiply(a, b) 
-   print(result) case "/": 
-   let result = div(a, b) 
-   print(result) 
+   print(result) case "/": do{
+     try let result = try div(a, b) 
+   print(result)    
+   } catch MyError.runtimeError(let msg) { print(msg) }
    default: print("error") 
   } 
 }
