43efddfc8fc56b7d301c671ff935efa382600c28
Олесь <102287993+olesosa@users.noreply.github.com>
2023-10-12 19:25:07 +0300

    Update lab.swift

--- lab.swift
+++ lab.swift
@@ -1,30 +1,52 @@
-enum MyError : Error{
-  case runtimeError(string)
+enum MyError: Error {
+    case runtimeError(String)
 }
 
-let s: String? = "/" let a:Int? = 205 let b:Int? = 36
+let s: String? = "/"
+let a: Int? = 205
+let b: Int? = 36
 
-func add(_ num1:Int, _ num2:Int)->Int{ return num1+num2 }
+func add(_ num1: Int, _ num2: Int) -> Int {
+    return num1 + num2
+}
 
-func subt(_ num1:Int, _ num2:Int)->Int{ return num1-num2 }
+func subt(_ num1: Int, _ num2: Int) -> Int {
+    return num1 - num2
+}
 
-func multiply(_ num1:Int, _ num2:Int)->Int{ return num1*num2 }
+func multiply(_ num1: Int, _ num2: Int) -> Int {
+    return num1 * num2
+}
 
-func div(_ num1:Int, _ num2:Int)->Int throws{ if b != 0 { return num1/num2 } else {throw MyError("error")} return 0}
+func div(_ num1: Int, _ num2: Int) throws -> Int {
+    if b != 0 {
+        return num1 / num2
+    } else {
+        throw MyError.runtimeError("error")
+    }
+}
 
 if let s = s, let a = a, let b = b {
-switch s 
-  { case "+": 
-   let result = add(a, b) 
-   print(result) 
-   case "-": 
-   let result = subt(a, b) 
-   print(result) case "*": 
-   let result = multiply(a, b) 
-   print(result) case "/": do{
-    try let result = div(a, b) 
-   print(result)    
-   } catch MyError.runtimeError(let msg) { print(msg) }
-   default: print("error") 
-  } 
+    switch s {
+    case "+":
+        let result = add(a, b)
+        print(result)
+    case "-":
+        let result = subt(a, b)
+        print(result)
+    case "*":
+        let result = multiply(a, b)
+        print(result)
+    case "/":
+        do {
+            let result = try div(a, b)
+            print(result)
+        } catch MyError.runtimeError(let msg) {
+            print(msg)
+        } catch {
+            print("Unknown error")
+        }
+    default:
+        print("error")
+    }
 }
